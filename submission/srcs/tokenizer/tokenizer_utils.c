9db3dbcf0f2407401d5170a2c47cd9384350aa90
HarukaOmura <harucha.cha.cha0723@gmail.com>
2026-04-04 14:35:10 +0900

    tokenizer見直し修正

--- submission/srcs/tokenizer/tokenizer_utils.c
+++ submission/srcs/tokenizer/tokenizer_utils.c
@@ -1,13 +1,15 @@
 #include "minishell.h"
+//! libftのft_isspaceを使うべき？？
 bool is_space(char c) {
 	return (c == ' ' || c == '\t');
 }
 
 // メタ文字 (区切り文字) かどうか
 // シェルにとって特別な意味を持つ文字(必須課題): | < >
+//! | < >だけでいいのか？
 bool is_metachar(char c)
 {
-    return (c == '|' || c == '<' || c == '>');
+    return (c == '|' || c == '<' || c == '>' || c == '$');
 }
 
 void consume_space(char **line) {
