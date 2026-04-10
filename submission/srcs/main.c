7b0eb7e9950196442210987be1cc5cc0b143dcad
hkuninag <hkuninag@student.42tokyo.jp>
2026-04-10 17:59:48 +0900

    環境変数の$ の展開を確認できるようにコードを修正、debug関連のコードをコメントアウト。

--- submission/srcs/main.c
+++ submission/srcs/main.c
@@ -3,6 +3,8 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
+static char	**g_envp;
+
 /*
 ** ループ処理を行う関数
 ** readlineはmallocされた文字列を返すので、使い終わったらfreeが必要です。
@@ -33,16 +35,18 @@ void    minishell_loop(t_shell *shell)
 		// 2. 字句解析 (Lexer)
 		shell->tokens = tokenize(line);
 		// [Debug] トークンの中身を見てみる（必要なときだけ呼び出し）
-		debug_print_tokens(shell->tokens);
+		// debug_print_tokens(shell->tokens);
 
 
 		//TODO  3. Parser : トークンを解析して shell->cmds に変換（今後実装）
 		shell->cmds = parse(shell->tokens);
 		// [Debug] パース結果（t_cmdリスト）を見たいときだけ有効化
-		debug_print_cmds(shell->cmds);
+		// debug_print_cmds(shell->cmds);
 
 		// $ 展開 (Expander)
 		ft_expand_args(shell);
+		if (shell->cmds && shell->cmds->args && shell->cmds->args[0])
+			execute_command(shell->cmds->args, g_envp);
 
 		//TODO 3. 実行 (Executor) コマンドを実行し、終了ステータスを更新（今後実装）
 		// shell->last_status = execute(shell);
@@ -65,11 +69,11 @@ int	main(int argc, char **argv, char **envp)
 	// shell = (t_shell){0};
 	shell = (t_shell){NULL, NULL, NULL, 0}; //わかりやすい
 	// shell = (t_shell){.env = NULL, .tokens = NULL, .cmds = NULL, .last_status = 0}; //可読性は上がるが長い
+	g_envp = envp;
 
 	//! 環境変数の取得。envpを解析して、扱いやすい連結リスト(t_env)に変換する
 	// TODO: (展開 (Expander)」の実装に取り掛かる直前)に実装
-	(void)envp;//後で削除
-	// shell.env = init_env(envp);
+	shell.env = init_env(envp);
 
 	//2. シグナル初期化
 	// setup_signals();
