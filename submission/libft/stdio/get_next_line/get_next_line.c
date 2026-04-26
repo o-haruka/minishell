2e9b9d4f31d14ece56ea407a4b6d66e04be0b000
Hisashi Kuninaga <hkuninag@c6r1s6.42tokyo.jp>
2026-04-26 16:53:35 +0900

    get_next line の main を削除し、make エラーを解消

--- submission/libft/stdio/get_next_line/get_next_line.c
+++ submission/libft/stdio/get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: homura <homura@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: hkuninag <hkuninag@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 15:16:02 by homura            #+#    #+#             */
-/*   Updated: 2025/08/12 20:51:21 by homura           ###   ########.fr       */
+/*   Updated: 2026/04/26 16:52:50 by hkuninag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,39 +37,4 @@ char	*get_next_line(int fd)
 	if (ft_putc(&ret, '\0') == -1)
 		return (ft_free(ret.str));
 	return (ret.str);
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	int fd;
-	char *line;
-	int line_count;
-	line_count = 0;
-
-	fd = open("text.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("open");
-		return (1);
-	}
-
-	printf("ファイルの内容を1行ずつ読み込みます:\n");
-	printf("========================================\n");
-
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		line_count++;
-		printf("行 %d: %s", line_count, line);
-
-		free(line);
-	}
-
-	printf("\n========================================\n");
-	printf("読み込み完了。総行数: %d行\n", line_count);
-
-	close(fd);
-	return (0);
 }
